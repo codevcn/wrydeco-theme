42cbb50d07ca943aba77a034cf8f86fde8fd7d5f
codevcn <codevoicainay@gmail.com>
2026-07-02 18:39:57 +0700

    strat to merge homepage

--- dev.cmd
+++ dev.cmd
@@ -6,5 +6,9 @@ echo Ban co the lay mat khau nay trong trang quan tri Shopify:
 echo Online Store -^> Preferences -^> Password protection.
 echo ===================================================
 echo.
-set /p STORE_PASSWORD="Vui long nhap mat khau cua hang (Store password): "
+REM Thay doi mat khau mac dinh o bien DEFAULT_STORE_PASSWORD
+set DEFAULT_STORE_PASSWORD=123456
+
+echo Tu dong su dung mat khau: %DEFAULT_STORE_PASSWORD%
+set STORE_PASSWORD=%DEFAULT_STORE_PASSWORD%
 python dev_wrapper.py --store-password=%STORE_PASSWORD% %*
